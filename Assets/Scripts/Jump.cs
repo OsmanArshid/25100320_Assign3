af1926b0fc13a813b9fbdaa026bebe6fc8114d5b
OsmanArshid <157830860+OsmanArshid@users.noreply.github.com>
2024-09-29 23:56:45 +0500

    update Jump.cs

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -8,6 +8,17 @@ public class Jump : MonoBehaviour
 {
 
     private CharacterController characterController;
+
+    private Vector3 _playerVelocity;
+
+    private bool _ground;
+    [SerializeField] private float jumpHeight = 5.0f;
+
+    private bool jumpPressed = false;
+
+    private float gravity = -9.8f;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +28,28 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovementJump();
     }
+
+    void MovementJump()
+    {
+        _ground = characterController.isGrounded;
+
+        if(_ground)
+        {
+        _playerVelocity.y = 0.0f;
+        }
+
+        if (jumpPressed && _ground){
+        _playerVelocity.y += Mathf.Sqrt(jumpHeight * -1.0f * gravity );
+        }
+    
+    }
+
+    
     void OnJump()
     {
-
+        if (characterController.velocity.y == 0){
+        jumpPressed = true;
     }
 }
